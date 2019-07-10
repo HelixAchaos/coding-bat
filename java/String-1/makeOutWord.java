9e97184aa5a8720a1da8d1c56574f75d0cc5d2d9
HelixAchaos <40044108+HelixAchaos@users.noreply.github.com>
2019-07-09 22:05:02 -0400

    Update makeOutWord.java

--- java/String-1/makeOutWord.java
+++ java/String-1/makeOutWord.java
@@ -1,3 +1,3 @@
 public String makeOutWord(String out, String word) {
-  return out.substring(0, out.length()/2) + word + out.substring(out.length()/2);
+  return out.substring(0, 2) + word + out.substring(2);
 }
